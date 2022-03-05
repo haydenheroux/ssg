2a2c2dcd67da3e79c4ba4fbbfaefd1e2f1af2d11
Hayden Heroux <haydenheroux@gmail.com>
2022-03-04 20:20:49 -0500

    Lay out the major steps in the algorithm behind the Generate function.

--- generate.go
+++ generate.go
@@ -1,10 +1,30 @@
 package main
 
 func Generate(source []string) (string, error) {
-	var output string
-	for _, line := range source {
-		output += line
-		output += "\n"
+	var final string
+
+	// Collect the source of a page into blocks based on content.
+	// E.g. paragraphs, code segments, or images
+	collected := collect(source)
+
+	// Wrap the blocks of the page in their respective tags.
+	// Additionally, add hints which describe the content of each block.
+	wrapped := wrap(collected)
+
+	// Format the source blocks to match arbitrary standards.
+	// These standards may include ideal line lengths per paragraph,
+	// automatic spell checking, and escaping of character sequences for
+	// friendlier alternatives.
+	formatted := format(wrapped)
+
+	// Embed the source blocks into an existing template, which may include
+	// headers, footers, style-sheets, and scripts.
+	embedded := embed(formatted)
+
+	for _, line := range embedded {
+		final += line
+		final += "\n"
 	}
-	return output, nil
+
+	return final, nil
 }
