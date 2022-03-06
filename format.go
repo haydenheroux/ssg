6868ac01b1f2e105e952b16639178b79317bef8f
Hayden Heroux <haydenheroux@gmail.com>
2022-03-05 23:56:55 -0500

    Formatting rule #1: Remove blank lines.

--- format.go
+++ format.go
@@ -1,5 +1,15 @@
 package main
 
 func format(lines []string) []string {
-	return lines
+
+	var output []string
+
+	for _, line := range lines {
+		if len(line) == 0 || (len(line) == 1 && line[0] == []byte("\n")[0]) {
+			continue
+		}
+		output = append(output, line)
+	}
+
+	return output
 }
