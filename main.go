9911602e3afbd70157677461a0bfbc03b516dbab
Hayden Heroux <haydenheroux@gmail.com>
2022-03-06 00:25:08 -0500

    Implement option for specifying the output file.

--- main.go
+++ main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"flag"
 	"io/ioutil"
 	"log"
-	"os"
 )
 
 func main() {
 
-	filePath := os.Args[1]
+	var outputFilePath string
 
-	lines, err := Read(filePath)
+	flag.StringVar(&outputFilePath, "o", "", "The path to the output file. Defaults to <inputFilePath>.html if left blank.")
+	flag.Parse()
+
+	inputFilePath := flag.Arg(0)
+	if len(inputFilePath) == 0 {
+		log.Fatalf("error while loading file: no file specified")
+	}
+
+	lines, err := Read(inputFilePath)
 	if err != nil {
 		log.Fatalf("error while loading file: %v", err)
 	}
@@ -20,7 +28,11 @@ func main() {
 		log.Fatalf("error while generating content: %v", err)
 	}
 
-	if err := ioutil.WriteFile(filePath+".html", []byte(mutated), 0666); err != nil {
+	if outputFilePath == "" {
+		outputFilePath = inputFilePath + ".html"
+	}
+
+	if err := ioutil.WriteFile(outputFilePath, []byte(mutated), 0666); err != nil {
 		log.Fatalf("error while writing file: %v", err)
 	}
 }
